06b6985b2961e50b24d4f9ea875813838653298b
DaphneBruggeman <0986809@hr.nl>
2023-06-09 11:52:53 +0200

    Kaart

--- src/pages/Simulation.js
+++ src/pages/Simulation.js
@@ -1,4 +1,12 @@
 import { useEffect, useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup
+} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import {Icon} from 'leaflet'
 
 function Simulation() {
   const [startSimulation, setStartSimulation] = useState(false);
@@ -9,9 +17,30 @@ function Simulation() {
     }
   }, [startSimulation]);
 
+  //map varable
+const center = [51.914489639203964, 4.469976900179068];
+
+const marker = [
+  {
+  geocode: [51.91672782942032, 4.483008152569202],
+  PopUp:"Verdrinking"
+  },
+  {
+    geocode: [51.91749120042259, 4.484958353291552],
+    PopUp:"AED"
+  }
+];
+
+const costomIcon = new Icon({
+  iconUrl: "https://cdn.pixabay.com/photo/2013/07/13/10/29/icon-157354_1280.png",
+  iconSize: [10]
+})
+
   return (
+    <>
+    {!startSimulation ? (
+
     <div className="h-screen grid justify-center flex-auto content-center">
-      {!startSimulation ? (
         <button
           className={"border-black border-2 p-0.5"}
           onClick={() => {
@@ -20,10 +49,47 @@ function Simulation() {
         >
           Start de simulatie
         </button>
+        </div>
+
       ) : (
-        <div className={"space-x-1"}>Hier komt de kaart</div>
+        <>
+        <MapContainer
+        center={center}
+        zoom={11}
+        style={{ width: '100%', height: '60vh'}}>
+    
+          <TileLayer
+          url='https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=XZFkelHKMGmz6pc20kRu'
+          >
+          </TileLayer>
+    
+          {marker.map(marker=>(
+            <Marker
+            position={marker.geocode}
+            >
+              <Popup>{marker.PopUp}</Popup>
+            </Marker>
+            ))}
+    
+        </MapContainer>
+        <div>
+            <h2>Vrouw te water</h2>
+            <h3>Wijnhaven 107, Rotterdam</h3>
+            <h4>*cords*</h4>
+            <br></br>
+            <p>AED</p>
+            <p>800m van je vandaan</p>
+            <br></br>
+            <form>
+             <button type="button">Annuleren</button>
+             <br></br>
+             <button type="submit">Doorverbinden</button>
+          </form>
+    
+        </div>
+        </>
       )}
-    </div>
+      </>
   );
 }
 
